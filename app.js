016f93ae3417abec7aacfb9e39668018001053a7
James Bowles <jamesrbowles@gmail.com>
2022-11-22 15:42:44 +0100

    generate colors func + difficulty

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let colors = [];
 const colorDisplay = document.getElementById("color-display");
 const squares = document.querySelectorAll(".square");
 const reset = document.getElementById("reset");
+const easy = document.querySelector(".easy")
+const hard = document.querySelector(".hard")
 
 // random RGB color generator from start
 
@@ -24,23 +26,47 @@ const genRandomColor = (num) => {
     return arr;
 }
 
-genRandomColor(numSquares)
+console.log(genRandomColor(numSquares))
+
+
+// Easy or Hard
+
+easy.addEventListener('click', function() {
+    if(!easy.classList.contains('selected')){
+        easy.classList.add('selected')
+        hard.classList.remove('selected')
+    }
+    numSquares = 3
+    for (let i = 3; i < squares.length; ++i) {
+        squares[i].classList.remove('square');
+     }
+     genColors()
+})
+
+
+hard.addEventListener('click', function() {
+    if(!hard.classList.contains('selected')){
+        hard.classList.add('selected')
+        easy.classList.remove('selected')
+    }
+    numSquares = 6
+    for (let i = 0; i < squares.length; ++i) {
+        squares[i].classList.add('square');
+     }
+     genColors()
+})
+
+// Generate random Colors
+
+const genColors = () => {
+    colors = genRandomColor(numSquares)
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i]
+    }
+}
 
 // random RGB from reset
 
-reset.addEventListener("click", function() {
-    colors = genRandomColor(numSquares)
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = genRandomColor(numSquares)
-    }
-});
+reset.addEventListener("click", genColors);
 
-/*
-// color guess function
-for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener("click", function() {
-        randomColor()
-        
-    },{once : true})
-}
-*/
+genColors()
